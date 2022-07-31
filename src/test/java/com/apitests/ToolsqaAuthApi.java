c858bc1d8adf26a8f13b33be1755ca2461f63d27
Kerim Goztepe <kerimgoztepe@yahoo.com>
2022-07-31 19:14:02 +0100

    updates

---
+++ src/test/java/com/apitests/ToolsqaAuthApi.java
@@ -0,0 +1,23 @@
+package com.apitests;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+import io.restassured.specification.RequestSpecification;
+
+public class ToolsqaAuthApi {
+
+    @Test
+    public void getData() {
+        RequestSpecification httpRequest = RestAssured.given();
+        Response res = httpRequest.get("https://postman-echo.com/basic-auth");
+        ResponseBody body = res.body();
+        //Converting the response body to string
+        String testApi = body.asString();
+        System.out.println("Data from the GET API- "+ testApi);
+
+    }
+}
+
