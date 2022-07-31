dbbe099943df14b75e75848934aa36cfd0e21949
Kerim Goztepe <kerimgoztepe@yahoo.com>
2022-07-31 18:35:00 +0100

    updates

---
+++ src/test/java/com/apitests/ReqresDeleteApiTest.java
@@ -0,0 +1,25 @@
+package com.apitests;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class ReqresDeleteApiTest {
+
+   String baseURL = "https://reqres.in/api";
+
+    @Test
+    public void deleteAllServicesApiTest() {
+        RestAssured.baseURI=baseURL;
+        Response response = given() //when().baseUri("https://reqres.in/api")
+                .header("Content-Type", "application/json")
+                .header("Authorization", "Bearer 65cbc4c43040ae97eb012d87fb6bb928809feb842bfeba5ea49b2969ea92ede6")
+                .pathParam("id", 13393)
+                .when()
+                .delete("/{id}");
+        response.then().statusCode(204);
+        response.prettyPrint();
+    }
+}
---
+++ src/test/java/com/apitests/ReqresGetApiTest.java
@@ -0,0 +1,25 @@
+package com.apitests;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+public class ReqresGetApiTest {
+
+    String baseURL = "https://reqres.in/api";
+
+    @Test
+    public void test1(){
+        // RestAssured.get(spartanBaseURL+"/api/spartans");
+        Response response = RestAssured.get(baseURL + "/users");
+
+        //print the status code
+        System.out.println(response.statusCode());
+
+
+        //print body
+        System.out.println(response.body().prettyPrint());
+    }
+
+
+}
